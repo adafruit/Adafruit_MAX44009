b30782ef8a3f4aadfafd60bb4770a8673478dfd6
clawpi <clawpi@adafruit.com>
2026-03-01 13:52:30 -0500

    Simpletest: 100ms rate, serial plotter friendly
    Co-authored-by: ladyada <limor@ladyada.net>

--- examples/max44009_simpletest/max44009_simpletest.ino
+++ examples/max44009_simpletest/max44009_simpletest.ino
@@ -2,7 +2,8 @@
  * @file max44009_simpletest.ino
  *
  * Simple example for the MAX44009 ambient light sensor.
- * Reads lux value every second in auto-ranging mode.
+ * Reads lux value every 100ms in auto-ranging mode.
+ * Output is Serial Plotter friendly.
  */
 
 #include <Adafruit_MAX44009.h>
@@ -16,8 +17,6 @@ void setup() {
     delay(10);
   }
 
-  Serial.println(F("MAX44009 Simple Test"));
-
   if (!max44009.begin()) {
     Serial.println(F("Could not find MAX44009 sensor!"));
     while (1) {
@@ -25,20 +24,15 @@ void setup() {
     }
   }
 
-  Serial.println(F("MAX44009 found!"));
+  Serial.println(F("Lux"));
 }
 
 void loop() {
   float lux = max44009.readLux();
 
-  if (max44009.isOverrange()) {
-    Serial.println(F("Sensor overrange!"));
-  } else if (isnan(lux)) {
-    Serial.println(F("Error reading lux"));
-  } else {
-    Serial.print(F("Lux: "));
+  if (!isnan(lux)) {
     Serial.println(lux);
   }
 
-  delay(1000);
+  delay(100);
 }
