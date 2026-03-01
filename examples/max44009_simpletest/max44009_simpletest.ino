1d6ffa793a2125c4b3164dc3c76caa977f77d796
clawpi <clawpi@adafruit.com>
2026-03-01 14:45:49 -0500

    Simpletest: move label into loop for plotter
    Co-authored-by: ladyada <limor@ladyada.net>

--- examples/max44009_simpletest/max44009_simpletest.ino
+++ examples/max44009_simpletest/max44009_simpletest.ino
@@ -1,35 +1,32 @@
-/*!
- * @file max44009_simpletest.ino
+/*
+ * Simple example for the MAX44009 Ambient Light Sensor
  *
- * Simple example for the MAX44009 ambient light sensor.
- * Reads lux value every 100ms in auto-ranging mode.
- * Output is Serial Plotter friendly.
+ * Written by Limor 'ladyada' Fried with assistance from Claude Code for
+ * Adafruit Industries. MIT license, check license.txt for more information
+ *
+ * Reads lux every 100ms in auto-ranging mode. Serial Plotter friendly.
  */
 
 #include <Adafruit_MAX44009.h>
-#include <Wire.h>
 
 Adafruit_MAX44009 max44009;
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial) {
+  while (!Serial)
     delay(10);
-  }
 
   if (!max44009.begin()) {
     Serial.println(F("Could not find MAX44009 sensor!"));
-    while (1) {
+    while (1)
       delay(10);
-    }
   }
-
-  Serial.println(F("Lux"));
 }
 
 void loop() {
   float lux = max44009.readLux();
 
+  Serial.print(F("Lux:"));
   if (!isnan(lux)) {
     Serial.println(lux);
   }
